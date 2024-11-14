7c8eeb813945d7e1b3d3abb594fb86ee0a03f256
TejasShukla274 <tejasshukla274@gmail.com>
2024-11-14 16:22:47 +0530

    Create f.c

---
+++ f.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+int a,b,c,i;
+printf("Enter your Account Number- ");
+    scanf("%d",&a);
+    printf("Enter your pin- ");
+    scanf("%d",&b);
+    if(b==1234){
+        printf("Your account balance is 749347 dollars");
+       
+       }
+       while(b!=1234){ 
+        printf("Enter your pin- ");
+    scanf("%d",&b);
+    
+        
+        }
+    
+}
